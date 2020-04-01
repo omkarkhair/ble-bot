a9d279724aa135244e98f95dd1b31b2fe867cc6a
Omkar Khair <omkarkhair@gmail.com>
2020-04-01 22:29:32 +0530

    Added server with web socket support

--- pi/index.js
+++ pi/index.js
@@ -1,4 +1,5 @@
 const { Gpio } = require( 'onoff' );
+var socket = require('socket.io-client')('http://localhost:3000');
 
 // set BCM 4 pin as 'output'
 const in1 = new Gpio('4', 'out')
@@ -14,9 +15,24 @@ in2.writeSync(0);
 in3.writeSync(0);
 in4.writeSync(0);
 
+
+socket.on('connect', function(){
+    console.log("Connected to weebo server");
+});
+
+socket.on('weebo-control', function(data){
+    console.log("Received instruction:", data);
+});
+
+socket.on('disconnect', function(){
+    console.log("Disconnected from weebo server");
+});
+
+/*
 // run a infinite interval
 setInterval( () => {
   in1.writeSync( isLedOn ? 0 : 1 ); // provide 1 or 0
   isLedOn = !isLedOn; // toggle state
   console.log("LED: ", isLedOn)
 }, 3000 ); // 3s
+*/
