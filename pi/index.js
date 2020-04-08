cb0e959728406dd67bba201c6b0cd82ac9c364c5
om <omkarkhair@gmail.com>
2020-04-08 18:11:02 +0100

    Camera support

--- pi/index.js
+++ pi/index.js
@@ -1,6 +1,25 @@
 const { Gpio } = require( 'onoff' );
 var socket = require('socket.io-client')('http://localhost:3000');
 
+var MjpegCamera = require('mjpeg-camera');
+
+// Create an MjpegCamera instance
+var camera = new MjpegCamera({
+  name: 'weebo-cam',
+  url: 'http://localhost:8081/'
+});
+
+// As frames come in, emit them in socket.io
+camera.on('data', function(frame) {
+//  console.log("Camera frame ready")
+  if (socket.connected) {
+    socket.emit('frame', frame.data.toString('base64'))
+  }
+});
+
+// Start streaming
+camera.start();
+
 // set BCM 4 pin as 'output'
 const in1 = new Gpio('4', 'out')
 const in2  = new Gpio( '17', 'out' );
